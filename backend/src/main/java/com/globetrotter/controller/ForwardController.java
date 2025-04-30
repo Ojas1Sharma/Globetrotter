dbe82116d8aba57b51f78e33c831307c0d83b88c
Ojas <ojas.sharma@caastle.com>
2025-04-30 23:34:34 +0530

    bug

--- backend/src/main/java/com/globetrotter/controller/ForwardController.java
+++ backend/src/main/java/com/globetrotter/controller/ForwardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ForwardController {
-    @RequestMapping(value = "/{[path:[^\\.]*}")
+    @RequestMapping(value = "/**/{path:[^\\.]*}")
     public String forward() {
         return "forward:/index.html";
     }
