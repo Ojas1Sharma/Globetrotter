48282a61ec30e0c46201f31602ee63683cb6c5f6
Ojas <ojas.sharma@caastle.com>
2025-05-01 00:23:20 +0530

    fix

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,3 +1,3 @@
 export const config = {
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://34.93.167.53:8080'
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://34.93.167.53:80'
 }; 
