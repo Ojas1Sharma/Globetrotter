4e6a797a0a7ce770a1926fff36fe8cb0a74a38dc
Ojas <ojas.sharma@caastle.com>
2025-04-30 22:56:30 +0530

    changed invite links

--- backend/src/main/java/com/globetrotter/service/impl/ChallengeServiceImpl.java
+++ backend/src/main/java/com/globetrotter/service/impl/ChallengeServiceImpl.java
@@ -22,8 +22,8 @@ public class ChallengeServiceImpl implements ChallengeService {
     @Autowired
     private UserRepository userRepository;
 
-    @Value("${app.base-url}")
-    private String baseUrl;
+    @Value("${app.challenge.share-url}")
+    private String shareUrl;
 
     @Override
     @Transactional
@@ -41,11 +41,11 @@ public class ChallengeServiceImpl implements ChallengeService {
 
         challenge = challengeRepository.save(challenge);
 
-        String shareUrl = baseUrl + "/challenge/" + challenge.getInviteCode();
+        String challengeUrl = shareUrl + "/challenge/" + challenge.getInviteCode();
         
         return new ChallengeResponse(
             challenge.getInviteCode(),
-            shareUrl,
+            challengeUrl,
             challenge.getChallengerScore(),
             challenge.getChallengerCorrectAnswers(),
             challenge.getChallengerIncorrectAnswers()
